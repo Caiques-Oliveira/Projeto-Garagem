36ef0f442ea611e5e290222b8d1de6b09378296d
Rafael de Oliveira <32688957+RafaelFrOliveira@users.noreply.github.com>
2017-10-20 20:10:35 -0200

    Add files via upload
    Estruturado na forma de funcoes

--- garagem.ino
+++ garagem.ino
@@ -1,50 +1,118 @@
+// Projeto de automacao residencial - Controle de Portao de Garagem
+// Grupo = Caique, Rafael, Wesley
+// Versao com controle apenas local, falta integrar comando remoto - MQTT
+// Data: 20.10.2017
 
+//-----------------------------------------------------------------------------
+// Inclusao de bibliotecas
+//-----------------------------------------------------------------------------
 #include <Servo.h>
 
-Servo servo;  
+//-----------------------------------------------------------------------------
+// Declaracao de variaveis
+//-----------------------------------------------------------------------------
 
+Servo portao;
 int pos = 0;
 const int Botao = 2;
 int estadoBotao = LOW;
 int lampada = 8;
+int estadoPortao = LOW; // LOW indica portao fechado, HIGH indica portao aberto
+int estadoLampada = LOW; // LOW indica lampada apagada, HIGH indica lampada acesa
 
-void setup() {
-  servo.attach(6);
-  servo.write(pos);  
+char estadoConexao [4] = {'conectado' , 'desconectado' , 'conectando' , 'enviando'}; // MQTT
+
+//-----------------------------------------------------------------------------
+// Prototipos de funcao
+//-----------------------------------------------------------------------------
+
+ void abrePortao();
+ void fechaPortao();
+ void acendeLampada();
+ void apagaLampada();
+ 
+ int reportaConexao(char state);
+ void recebeMensagem();
+ void enviaMensagem();
+ 
+
+//-----------------------------------------------------------------------------
+// Inicializacao
+//-----------------------------------------------------------------------------
+ 
+void setup() 
+{
+  portao.attach(6);
+  portao.write(pos);  
   pinMode(Botao, INPUT);
   pinMode (lampada, OUTPUT);
   Serial.begin (9600);
 }
 
-void loop() {
+//-----------------------------------------------------------------------------
+// Principal
+//-----------------------------------------------------------------------------
 
- estadoBotao = digitalRead(Botao);
+void loop()
+{
+
+  estadoBotao = digitalRead(Botao);
   
   if (estadoBotao == HIGH)
   {
-   delay(100);
+   delay(100);//debounce
+   
    if (pos < 30)
-   {   
-   servo.write (90);
-   delay (1000);  
-   pos = 90;
-   digitalWrite (lampada, HIGH);
-   Serial.println ("Portão Aberto");
-   }
-   else
-   {
-    servo.write (0);
-    delay (1000);
-    pos = 0;
-    digitalWrite (lampada,LOW);
-    Serial.println ("Portão Fechado");
-   }
- 
-   
-   
+      { 
+          abrePortao();
+          acendeLampada();             
+      }
+      
+   else if (pos >60)
+      {
+          fechaPortao();
+          apagaLampada();                 
+      }   
   }
-
 }
 
+//-----------------------------------------------------------------------------
+// Funcoes auxiliares
+//-----------------------------------------------------------------------------
 
+ void abrePortao()
+ {
+    pos = 90; 
+    portao.write (pos);
+    delay (1000);         
+    Serial.println ("Portão Aberto");
+    estadoPortao = HIGH;  
+ }
+ 
+ void fechaPortao()
+ {
+    pos = 0;  
+    portao.write (pos);
+    delay (1000);    
+    Serial.println ("Portão Fechado");
+    estadoPortao = LOW; 
+ }
+ 
+ void acendeLampada()
+ {
+    digitalWrite (lampada, HIGH);
+    Serial.println ("Lampada Acesa");
+    estadoLampada = HIGH;  
+ }
+ 
+ void apagaLampada()
+ {
+    digitalWrite (lampada,LOW);
+    Serial.println ("Portão Apagada");
+    estadoLampada = LOW;  
+ }
+ 
+//-----------------------------------------------------------------------------
+// EOF
+//-----------------------------------------------------------------------------
 
