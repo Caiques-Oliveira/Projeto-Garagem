5d55b8a17ba5d77163a1e619a504d5bf33f930c5
Caiques-Oliveira <32913712+Caiques-Oliveira@users.noreply.github.com>
2017-10-24 21:50:48 -0200

    Add files via upload

---
+++ TESTE_24.10.ino
@@ -0,0 +1,57 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
+
+
+int lampada = 8;
+char portao;
+
+
+
+byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xF1, 0xE2 };
+
+
+void callback(char* topic, byte* payload, unsigned int length)
+{
+
+  if (topic == portao)
+  {
+    if (payload == 1)
+    {
+      lampada = HIGH;
+    }
+  }
+
+}
+void callback(char* topic, byte* payload, unsigned int length);
+EthernetClient ethClient;
+PubSubClient client("m12.mqttcloud.com", 14713, callback, ethClient);
+
+
+
+
+void setup()
+{
+  Ethernet.begin(mac);
+  if (client.connect("arduinoClient", "teste", "teste")) {
+    //client.publish("outTopic","hello world");
+    client.subscribe("inTopic");
+  }
+
+  pinMode (lampada, OUTPUT);
+
+}
+
+
+void loop() {
+  client.loop();
+
+
+}
+
+
+
+
+// put your main code here, to run repeatedly:
+
+
